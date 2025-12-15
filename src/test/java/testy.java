4ea9c007f1eb7b10dcfc4d496bb91baf4d7e2d89
ADIK-318 <adikub318@wp.pl>
2025-12-15 20:48:06 +0100

    Update testy.java
    Dodanie 2 testów

--- src/test/java/testy.java
+++ src/test/java/testy.java
@@ -10,4 +10,16 @@ class testy {
         int result = Main.add(2, 3);
         assertEquals(5, result);
     }
+    
+    @Test
+    void shouldAddTwoNumbers2() {
+        int result = Main.add(2, 3);
+        assertEquals(5, result);
+    }
+    
+    @Test
+    void shouldAddTwoNumbers3() {
+        int result = Main.add(2, 3);
+        assertEquals(5, result);
+    }
 }
