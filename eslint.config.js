6304f735ca53665bba59df8d97331127b3a616f9
ADIK-318 <adikub318@wp.pl>
2026-01-07 10:47:58 +0100

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs"
+    },
+    rules: {
+      "no-undef": "error",
+      "no-unused-vars": "error"
+    }
+  }
+];
